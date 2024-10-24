3a9dd870976c3f9a1231a59c5def8dcd65655df9
manoj <msbarhate14@gmail.com>
2024-10-24 11:34:49 +0530

    Cucumber framework in Intellij idea with jenkins integration

--- src/test/java/stepDefinations/StepDefFeature1.java
+++ src/test/java/stepDefinations/StepDefFeature1.java
@@ -5,7 +5,7 @@ public class StepDefFeature1 {
 
     @Given("user navigate to home page")
     public void user_navigate_to_home_page() {
-
+        System.out.println("first line");
     }
     @When("enter the deatils in form")
     public void enter_the_deatils_in_form() {
