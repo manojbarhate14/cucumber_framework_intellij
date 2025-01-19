69653e3fce0563bd14d1c1b03100f41037593f16
manoj <msbarhate14@gmail.com>
2025-01-19 09:12:33 +0530

    Cucumber framework in Intellij idea with jenkins integration

--- src/main/java/InterviewPrograms/Practice.java
+++ src/main/java/InterviewPrograms/Practice.java
@@ -1,12 +1,11 @@
 package InterviewPrograms;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Practice {
     public static void main(String[] args) {
 
-        String word = "swisshttodft";
 
-        for (char i : word.toCharArray()) {
-            System.out.println(word.indexOf(i)+" ,"+word.lastIndexOf(i));
-        }
     }
 }
--- src/main/java/InterviewPrograms/ReverseString.java
+++ src/main/java/InterviewPrograms/ReverseString.java
@@ -1,11 +1,14 @@
 package InterviewPrograms;
 
+import java.util.Arrays;
+
 public class ReverseString {
 
     public static void main(String[] args) {
         ReverseString obj = new ReverseString();
-        obj.wordwiseReverseString();
-        obj.reverseAllString();
+        // obj.wordwiseReverseString();
+        // obj.reverseAllString();
+        reverseEachWordOnSamePlace();
     }
 
     public void wordwiseReverseString() {
@@ -18,12 +21,26 @@ public class ReverseString {
         System.out.println();
     }
 
+    public static void reverseEachWordOnSamePlace()
+    {
+        String str = "Selenium and Java";
+        String[] words = str.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = new StringBuilder(words[i]).reverse().toString();
+        }
+
+        String reversedString = String.join(" ", words);
+
+        System.out.println("Reversed string: " + reversedString);
+    }
+
     public void reverseAllString() {
         String str = "Selenium and Java";
         String reverse = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
-            reverse = reverse + str.charAt(i);
+            reverse =reverse+str.charAt(i);
         }
         System.out.print(reverse);
     }
